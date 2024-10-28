b402958404c7ebc5e4a8b39cbb889f0f09280135
anderferr <andersonferreirao.oficial@gmail.com>
2024-10-28 19:04:05 -0300

    docker configurado para ETL-1

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+"""importando as libs"""
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
+    """return value"""
     return {"message": "Hello World"}
---
+++ run.sh
@@ -0,0 +1,6 @@
+pylint app.py --disable=import-error
+docker compose build
+docker compose up
+
+# docker compose run server pylint app.py
+# docker compose run bash
